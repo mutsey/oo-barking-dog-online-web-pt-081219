8630110e834fdbbbf74d8fd27167d25c19a0147d
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-24 23:07:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog 
+   def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+    Casper = Dog.new
+Casper.name = "Casper"
+ 
+Casper.name #=> "Casper"
+  end
+   #class body
+  
+  #Instance Method Definition 
+  def bark 
+    puts "Woof!"
+  end
+end
